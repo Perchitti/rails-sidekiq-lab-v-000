c1200057bbf557c082587ea422a582a9f4f4c32e
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-08 12:29:48 +0000

    automatically backed up by learn

--- app/workers/songs_workers.rb
+++ app/workers/songs_workers.rb
@@ -3,8 +3,8 @@ class SongsWorker
   include SideKiq::Worker
 
   def perform(songs_file)
-    CSV.foreach(songs_file, headers: true) do |t|
-      Song.create(title: t[0], artist_name: [1])
+    CSV.foreach(songs_file, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
     end
   end
 
